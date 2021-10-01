7cdb35faf6422b248c9b25d815257f65082f5a29
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-10-01 19:27:47 +0530

    Update dependencies, remove onesignal, update url

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:udit_jharkhand/splash.dart';
 
-
-final flutterWebViewPlugin = FlutterWebviewPlugin();
-
-void main(){
-runApp(MaterialApp(
-      home: Splash(),
-  ));
-
-
-
+void main() {
+  runApp(MaterialApp(home: Splash()));
 }
 
 class MyApp extends StatefulWidget {
-
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    //OneSignal.shared.promptUserForPushNotificationPermission();
-
-    OneSignal.shared.init(
-      "4c36ab5f-ed3f-41f7-aa79-9021fa9b7b6c",
-
-    );
-    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
-
-    OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
-      // will be called whenever a notification is received
-      print(notification);
-    });
-  }
-
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue
-      ),
-
-    home: SafeArea(
-      child:  WebviewScaffold(
-        appBar: AppBar(
-          title: Text('Web View App'),
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: SafeArea(
+        child: WebviewScaffold(
+          appBar: AppBar(
+            title: Text('Web View App'),
+          ),
+          url: 'https://digiwrecks.com/',
+          debuggingEnabled: true,
+          initialChild: Center(
+            child: CircularProgressIndicator(),
+          ),
+          withJavascript: true,
+          withZoom: true,
+          allowFileURLs: true,
+          appCacheEnabled: true,
+          displayZoomControls: true,
         ),
-        url: 'https://www.uditjharkhand.com',
-        debuggingEnabled: true,
-        initialChild: Center(child: CircularProgressIndicator(),),
-        withJavascript: true,
-        withZoom: true,
-        allowFileURLs: true,
-        appCacheEnabled: true,
-        displayZoomControls: true,
-
       ),
-    ),
-
-
     );
   }
 }
